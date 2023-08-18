7451efb971339bab42596c3757f2397a8a8d668f
Leonardo Fuchs <Leonardo.santosfuchs07@gmail.com>
2023-08-18 02:57:16 -0300

    Segundo commit

---
+++ servico/src/test/java/br/com/osguri/servico/ServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.osguri.servico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
